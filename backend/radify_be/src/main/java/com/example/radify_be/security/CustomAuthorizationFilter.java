fa8848212f4551c3bccd7052310ed596e0408e90
Radolina Petrova <478897@student.fontys.nl>
2023-01-09 04:08:50 +0100

    Touch ups

--- backend/radify_be/src/main/java/com/example/radify_be/security/CustomAuthorizationFilter.java
+++ backend/radify_be/src/main/java/com/example/radify_be/security/CustomAuthorizationFilter.java
@@ -5,8 +5,6 @@ import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.interfaces.DecodedJWT;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
